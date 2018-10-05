ae4826a2c0d7ca6bb072ec885bdfc52dab059e26
TAKII KENSUKE <takii.kensuke.26z@st.kyoto-u.ac.jp>
2018-10-05 14:05:42 +0900

    task2 first commit

--- sample1_new/sample1.py
+++ sample1_new/sample1.py
@@ -28,7 +28,14 @@ def softmax(a):
     for i in range(CLASS):
         den_y2 += np.exp(a[i] - alpha)
     y2 = np.exp(a - alpha) / den_y2
-    return np.argmax(y2)
+    return y2
+
+# Cross entropy error (as loss function)
+def cEntropy(y, y2):
+    entropy = 0
+    for i in range(y.size):
+        entropy -= y[i] * np.log(y2[i])
+    return entropy
 
 def layer(seed, x, co_y, afun):
     np.random.seed(seed)
@@ -39,16 +46,23 @@ def layer(seed, x, co_y, afun):
     t = W.dot(x) + b
     return afun(t)
 
+# Loss function
+# Converts integer to one-hot vector and calc loss
+def lossFun(y, y2):
+    y_arr = np.zeros(10)
+    y_arr[y] = 1
+    return cEntropy(y_arr, y2)
+
 
 # =========================================
 # Execution Unit
 # =========================================
 
-idx = input("Please enter a number (0-9999): ")
-idx = int(idx)
-if idx >= 0 and idx < PIC_TEST:
+batch = input("How many batches do you want to use? (0-999): ")
+batch = int(batch)
+if batch >= 0 and batch < PIC_LEARN:
     # Preprocessing
-    X, Y = mndata.load_testing()
+    X, Y = mndata.load_training()
     X = np.array(X)
     X = X.reshape((X.shape[0],SIZEX,SIZEY))
     Y = np.array(Y)
@@ -58,14 +72,25 @@ if idx >= 0 and idx < PIC_TEST:
     #plt.imshow(X[idx], cmap=cm.gray)
     #plt.show()
 
-    # Input layer
-    # Convert the image data to a vector which has (SIZEX * SIZEY) dims
-    x = X[idx].ravel()
-    x = np.matrix(x).T
+    # Choose batches randomly
+    arr_idx = np.random.choice(PIC_LEARN, batch)
 
-    y1 = layer(5, x, M, sigmoid)        # Output from intermediate layer
-    a = layer(10, y1, CLASS, softmax)   # Output from output layer
-    print a
+    entropy_ave = 0
+
+    for idx in arr_idx:
+        # Input layer
+        # Convert the image data to a vector which has (SIZEX * SIZEY) dims
+        x = X[idx].ravel()
+        x = np.matrix(x).T
+
+        y1 = layer(5, x, M, sigmoid)        # Output from intermediate layer
+        y2 = layer(10, y1, CLASS, softmax)   # Output from output layer
+        # print y2
+
+        entropy_ave += lossFun(Y[idx], y2)
+
+    entropy_ave = entropy_ave / batch
+    print entropy_ave
 
 else:
     print ("Illegal Input!")
